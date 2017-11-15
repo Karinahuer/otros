c648f6137b40b845bf1b0c4623aa5c74d526af56
Karina Huerta <karina.huerta@klauds.net>
2017-11-14 22:28:58 -0600

    Add files via upload

---
+++ buquesOK-Noviembre14/investigacion-art.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+	<head>
+		<title>INICIO</title>
+		<meta http-equiv="description" content="page description" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	</head>
+	<body>
+		<?php include("1top-section.html");?>
+
+		<?php include("content-investigaciones-art1.html");?>
+
+		<?php include("2footer.html");?>
+
+	</body>
+</html>
